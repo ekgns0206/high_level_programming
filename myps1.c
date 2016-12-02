491f222752f1cd956e4055d87df0a2a0a4f43048
Minsik Jeon <alstlr1585@kpu.ac.kr>
2016-12-03 01:31:23 +0900

    2016-12-03 option no tty, no sub ps / tomorrow run tty really

--- myps1.c
+++ myps1.c
@@ -6,14 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <utmpx.h>
 #include <fcntl.h>
 #include <procfs.h>		// psinfo header
+#include <pwd.h>
 
 // PROC_EVENT 프로세스 찾을때마다 호출되는 형식
 // pid 프로세스 아이디
 // return 그만 찾으려면 1, 계속 찾으려면 0
 //
 typedef int (*PROC_EVENT)(struct psinfo pinfo, char option);
+
+extern char *optarg;	// option 인자
+
+uid_t uid;	//myuid
 
 // isNum 숫자로 구성된 문자열인지 검사
 // value 문자열
@@ -73,52 +79,88 @@ int enumProcess(PROC_EVENT callback, char option)
 int printProcess(struct psinfo pinfo, char option)
 {
 	int tty;
-	uid_t uid;
-	uid = getuid();
+	struct passwd *pw;
+	char ttyString[256] = {0,};
+	char tmp[256] = {0,};
+	
+	pw = getpwuid(pinfo.pr_uid);
+	
 	tty = pinfo.pr_ttydev;
+	
 	switch(option){
 		case 0:		// no option
+			if(uid == pinfo.pr_uid){	// no tty different
+				printf("%d\t", pinfo.pr_pid);
+				printf("%d\t", tty);	//tty
+				printf("%2d:", pinfo.pr_time.tv_sec/60);
+				printf("%02d\t", pinfo.pr_time.tv_sec%60);
+				printf("%s\n", pinfo.pr_fname);
+			}
 			break;
-		case 'a':
-			if (!strcmp(pinfo.pr_fname, "bash") || !strcmp(pinfo.pr_fname, "ksh"))
-				return 0;
-			if(tty > 6291456){
-				printf("%d\tpts/%d\t\t%d:%02d\t%s\n",
-				pinfo.pr_pid, tty-6291456, pinfo.pr_time.tv_sec/60,
-				pinfo.pr_time.tv_sec%60, pinfo.pr_fname);
+		case 'j':
+			if(uid == pinfo.pr_uid){	// no tty different
+				printf("%d\t", pinfo.pr_pid);
+				printf("%d\t", pinfo.pr_pgid);
+				printf("%d\t", pinfo.pr_sid);
+				printf("%d\t", tty);	//tty
+				printf("%2d:", pinfo.pr_time.tv_sec/60);
+				printf("%02d\t", pinfo.pr_time.tv_sec%60);
+				printf("%s\n", pinfo.pr_fname);
 			}
 			break;
 		case 'e':
-			if(tty == -1){
-				printf("%d\t?\t\t%d:%02d\t%s\n",
-				pinfo.pr_pid, pinfo.pr_time.tv_sec/60,
-				pinfo.pr_time.tv_sec%60, pinfo.pr_fname);
-			}
-			else if (tty == 0){
-				printf("%d\tconsole\t\t%d:%02d\t%s\n",
-				pinfo.pr_pid, pinfo.pr_time.tv_sec/60,
-				pinfo.pr_time.tv_sec%60, pinfo.pr_fname);
-			}
-			else if (tty < 6291456){
-				printf("%d\t??\t\t%d:%02d\t%s\n",
-				pinfo.pr_pid, pinfo.pr_time.tv_sec/60,
-				pinfo.pr_time.tv_sec%60, pinfo.pr_fname);
-			}
-			else{
-				printf("%d\tpts/%d\t\t%d:%02d\t%s\n",
-				pinfo.pr_pid, tty-6291456, pinfo.pr_time.tv_sec/60,
-				pinfo.pr_time.tv_sec%60, pinfo.pr_fname);
-			}
+			printf("%d\t", pinfo.pr_pid);
+			printf("tty\t");	//tty
+			printf("%2d:", pinfo.pr_time.tv_sec/60);
+			printf("%02d\t", pinfo.pr_time.tv_sec%60);
+			printf("%s\n", pinfo.pr_fname);
 			break;
 		case 'f':
+			printf("%s\t", pw->pw_name);
+			printf("%d\t", pinfo.pr_pid);
+			printf("%d\t", pinfo.pr_ppid);
+			printf("%d\t", pinfo.pr_argc);
+			printf("%02d:", ((pinfo.pr_start.tv_sec/3600)+9)%24);	// not today no code
+			printf("%02d:", (pinfo.pr_start.tv_sec%3600)/60);
+			printf("%02d\t", pinfo.pr_start.tv_sec%60);
+			printf("tty\t"); // tty space
+			printf("%02d:", pinfo.pr_time.tv_sec/60);
+			printf("%02d\t", pinfo.pr_time.tv_sec%60);
+			printf("%s\n", pinfo.pr_fname);
 			break;
-		case 'l':
+		case 's':
+			if(atoi(optarg) == pinfo.pr_sid){
+				printf("%d\t", pinfo.pr_pid);
+				printf("%d\t", tty);	//tty
+				printf("%2d:", pinfo.pr_time.tv_sec/60);
+				printf("%02d\t", pinfo.pr_time.tv_sec%60);
+				printf("%s\n", pinfo.pr_fname);
+			}
+			break;
+		case 'A':
+			printf("%d\t", pinfo.pr_pid);
+			printf("tty\t");	//tty
+			printf("%2d:", pinfo.pr_time.tv_sec/60);
+			printf("%02d\t", pinfo.pr_time.tv_sec%60);
+			printf("%s\n", pinfo.pr_fname);
+			break;
+		case 'g':
+			if(atoi(optarg) == pinfo.pr_pgid){
+				printf("%d\t", pinfo.pr_pid);
+				printf("%d\t", tty);	//tty
+				printf("%2d:", pinfo.pr_time.tv_sec/60);
+				printf("%02d\t", pinfo.pr_time.tv_sec%60);
+				printf("%s\n", pinfo.pr_fname);
+			}
 			break;
 		case 'u':
-			break;
-		case 'v':
-			break;
-		case 'x':
+			if(!strcmp(optarg, pw->pw_name)){
+				printf("%d\t", pinfo.pr_pid);
+				printf("%d\t", tty);	//tty
+				printf("%2d:", pinfo.pr_time.tv_sec/60);
+				printf("%02d\t", pinfo.pr_time.tv_sec%60);
+				printf("%s\n", pinfo.pr_fname);
+			}
 			break;
 	}
 }
@@ -126,39 +168,45 @@ int printProcess(struct psinfo pinfo, char option)
 int main(int argc, char **argv)
 {
 	int n;
-	extern char *optarg;
 	extern int optind;
 	char option;
-	
+
+	uid = getuid();
+
 	if(argc == 1){	// no option	
 		printf("PID\tTTY\tTIME\tCMD\n");
 		enumProcess(printProcess, 0);	
 	}
-
-	while((n = getopt(argc, argv, "aefluvx")) != 1){
+	
+	while((n = getopt(argc, argv, "jefAg:s:u:")) != 1){
 		switch(n){
-			case 'a':
-				option = 'a';
-				printf("PID\tTTY\t\tTIME\tCMD\n");
+			case 'j':
+				printf("PID\tPGID\tSID\tTTY\tTime\tCMD\n");
+				option = 'j';
 				break;
 			case 'e':
-				printf("PID\tTTY\t\tTIME\tCMD\n");
+				printf("PID\tTTY\tTIME\tCMD\n");
 				option = 'e';
 				break;
 			case 'f':
+				printf("UID\tPID\tPPID\tC\tSTIME\t\tTTY\tTIME\tCMD\n");
 				option = 'f';
 				break;
-			case 'l':
-				option = 'l';
+			case 's':	// sid가 인자와 같은 프로세스만 출력	
+				printf("PID\tTTY\tTIME\tCMD\n");
+				option = 's';
+				break;
+			case 'A':
+				printf("PID\tTTY\tTIME\tCMD\n");
+				option = 'A';
+				break;
+			case 'g':	// pgid가 인자와 같은 프로세스만 출력	
+				printf("PID\tTTY\tTIME\tCMD\n");
+				option = 'g';
 				break;
 			case 'u':
+				printf("PID\tTTY\tTIME\tCOMD\n");
 				option = 'u';
-				break;
-			case 'v':
-				option = 'v';
-				break;
-			case 'x':
-				option = 'x';
 				break;
 		}
 		enumProcess(printProcess, option);
@@ -167,4 +215,3 @@ int main(int argc, char **argv)
 
 	return 0;	
 }
-
